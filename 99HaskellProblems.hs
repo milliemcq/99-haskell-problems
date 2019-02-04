e8f03a5e9c402b010d2c9990d2db6bb3374c9ca8
Your Name Here <millie@mcquillin.com>
2019-02-04 15:14:11 +0000

    added scripts

---
+++ 99HaskellProblems.hs
@@ -0,0 +1,14 @@
+-- P1 Find the last element in a list
+lastElem xs = head (reverse xs)
+
+-- P2 Find the last bit one element in a list
+lastButOne xs = xs !! (length xs - 2)
+
+-- P3 Find the Kth element of a list
+elementAt xs i = xs !! i
+
+-- P4 Find the number of elements in a list
+myLength xs = length xs
+
+-- P5 Reverse a list/ reverse without using reverse 
+myReverse xs = reverse xs
