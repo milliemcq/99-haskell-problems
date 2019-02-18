66b42ba08ff4a406a53314674bb7f9e2564d544b
Your Name Here <millie@mcquillin.com>
2019-02-18 10:44:07 +0000

    Adding scalar

--- practical_3.hs
+++ practical_3.hs
@@ -6,6 +6,16 @@ compress (x:xs) = x:[ y | (x, y) <- (pairsxs xs), x /= y]
 pairsxs xs = zip xs (tail xs)
 
 -- '<-' Means taking elements from this list
+-- pyths = []
+-- for x in range(1,n):
+--   for y in range(1,n):
+--     for z in range(1,n):
+--       if x**2 + y**2 == z**2:
+--         pyths.append((x,y,z))
+
 pyths x = [(x, y, z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
 
-pyths n = [(x, y, z) | (x, y, z) <- (list), x^2 + y^2 == z^2]
+
+-- scalar product of two lists
+
+scalar xs ys = [x | x <- xs, y <- ys]
