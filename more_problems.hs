4724e2221f3e75654c718f800b2b0071748d8431
Your Name Here <millie@mcquillin.com>
2019-03-28 06:43:59 +0000

    First problems added

--- more_problems.hs
+++ more_problems.hs
@@ -1,13 +1,11 @@
 -- Quicksort (doesn't work)
- qsort [] = []
- qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
-                where
+qsort []     = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+               where
                   smaller = [a | a <- xs, x <= x]
                   larger = [b | b <- xs, b > x]
 
--- Quicksort in reverse
--- qsortReverse [] = []
--- qsortReverse (x:xs) = qsort smaller ++ [x] ++ qsort larger
---                where
---                  smaller = [a | a <- xs, x >= x]
---                  larger = [b | b <- xs, b < x]
+--My First Script
+double x = x + x
+
+quadruple x = double(double x)
