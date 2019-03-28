fa5cfb27813e34608abb00ecbb66ec7120ec73d2
Your Name Here <millie@mcquillin.com>
2019-03-28 06:51:52 +0000

    Broken code fixed

--- more_problems.hs
+++ more_problems.hs
@@ -9,3 +9,9 @@ qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
 double x = x + x
 
 quadruple x = double(double x)
+
+--Broken to be fixed
+funcN = a `div` length xs
+  where
+    a = 10
+    xs = [1, 2, 3, 4, 5]
