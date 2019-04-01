3f7b3c2d32f79e24b88832f1bd2c0310c66daeaf
Your Name Here <millie@mcquillin.com>
2019-04-01 09:01:16 +0100

    Compiles

--- ceaser.hs
+++ ceaser.hs
@@ -8,5 +8,5 @@ int2let :: Int -> Char
 int2let n = chr (ord 'a' + n)
 
 shift :: Int -> Char -> Char
-shift n c | isLower c = int2let((let2int c + n) 'mod' 26)
-          | otherwise c
+shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
+          | otherwise = c
