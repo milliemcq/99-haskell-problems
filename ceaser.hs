22787f3503298c14e72e0f17d02aa4c65b85fbc1
Your Name Here <millie@mcquillin.com>
2019-04-01 08:58:56 +0100

    Shift added

--- ceaser.hs
+++ ceaser.hs
@@ -1,7 +1,12 @@
+import Data.Char
 -- A ceaser cipher and decoder in Haskell
 
 let2int :: Char -> Int
-let2int = ord c - ord 'a'
+let2int c = ord c - ord 'a'
 
 int2let :: Int -> Char
 int2let n = chr (ord 'a' + n)
+
+shift :: Int -> Char -> Char
+shift n c | isLower c = int2let((let2int c + n) 'mod' 26)
+          | otherwise c
