70377ee8b8242f5a41d69177cd88b66bb5bb4f37
Your Name Here <millie@mcquillin.com>
2019-04-02 16:06:40 +0100

    Encoding Complete

--- ceaser.hs
+++ ceaser.hs
@@ -10,3 +10,6 @@ int2let n = chr (ord 'a' + n)
 shift :: Int -> Char -> Char
 shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
           | otherwise = c
+
+encode :: Int -> String -> String
+encode n xs = [shift n x | x <- xs]
