1dee6934acb90f6b92e98cb3f15a497e0253efd3
Your Name Here <millie@mcquillin.com>
2019-04-11 07:50:19 +0100

    Finished 4.8

--- exercises.hs
+++ exercises.hs
@@ -38,6 +38,11 @@ multLambda = (\x -> \y -> \z -> x*y*z)
 luhnDouble :: Int -> Int
 luhnDouble x = if (x*2) > 9 then ((x*2) - 9) else (x*2)
 
+-- 4.9 Define a function luhn that decides if a 4 digit bank card number is valid
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn u x y z | (((luhnDouble u + luhnDouble x + luhnDouble y + luhnDouble z) `mod` 10) == 0) = True
+             | otherwise = False
+
 
 
 -- Ch 6 Recursion
