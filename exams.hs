b0bef9059bf94f197901f64b970e3b93ccb6a2e4
Your Name Here <millie@mcquillin.com>
2019-04-28 12:15:18 +0100

    Works

--- exams.hs
+++ exams.hs
@@ -1,4 +1,4 @@
-mystery1 = 1 : map (*2) mystery 1
-mystery2 = 1 : map (*2) mystery 1
+mystery1 = 1 : map (*2) mystery1
+mystery2 = 1 : map (*2) mystery1
 
-mapTest = 1 : map (+) [1, 2, 3, 4]
+mapTest = 1 : map (+1) mapTest
