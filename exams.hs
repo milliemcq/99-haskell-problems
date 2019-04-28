40a72260d389638b9e7ef9a884f8390384ebaa5e
Your Name Here <millie@mcquillin.com>
2019-04-28 13:29:41 +0100

    Changes

--- exams.hs
+++ exams.hs
@@ -2,3 +2,7 @@ mystery1 = 1 : map (*2) mystery1
 mystery2 = 1 : map (*2) mystery1
 
 mapTest = 1 : map (+1) mapTest
+
+my_const c x = c
+my_append [] ys = ys
+my_append (x:xs) ys = x : my_append xs ys
